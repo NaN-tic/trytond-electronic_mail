39be626fd714a0c31ceb43037deb2fe4c37a1cf2
Guillem Barba <guillem@nan-tic.com>
2016-04-12 14:17:33 +0200

    Refactorized tests to follow the new API

--- tests/test_electronic_mail.py
+++ tests/test_electronic_mail.py
@@ -1,25 +1,61 @@
-# -*- coding: UTF-8 -*-
-#This file is part electronic_mail module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part electronic_mail module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 "Electronic Mail test suite"
-
 import unittest
-
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import formatdate
 
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import ModuleTestCase
-from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
-from trytond.transaction import Transaction
 from trytond.config import config
+from trytond.pool import Pool
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
+from trytond.transaction import Transaction
 
 # Set a data path since the module stores email attachment content in data dir
 config.set('database', 'path', '/tmp/')
 
 USER_TYPES = ('owner_user_%s', 'read_user_%s', 'write_user_%s')
+
+
+def create_user(name):
+    """
+    Creates a new user and returns it
+    """
+    pool = Pool()
+    ModelData = pool.get('ir.model.data')
+    User = pool.get('res.user')
+
+    group_email_admin_id = ModelData.get_id(
+        'electronic_mail', 'group_email_admin')
+    group_email_user_id = ModelData.get_id(
+        'electronic_mail', 'group_email_user')
+
+    return User.create([{
+                'login': name,
+                'name': name,
+                'password': name,
+                'groups': [
+                    ('add', [group_email_admin_id, group_email_user_id]),
+                    ],
+                }])[0]
+
+
+def create_users(no_of_sets=1):
+    """
+    A set of user is:
+        1 Owner
+        1 Read user
+        1 Write User
+    :return: List of tuple of three users
+    """
+    created_users = []
+    for iteration in xrange(1, no_of_sets + 1):
+        created_users.append(
+            tuple(create_user(user_type % iteration)
+                for user_type in USER_TYPES))
+    return created_users
 
 
 class ElectronicMailTestCase(ModuleTestCase):
@@ -28,92 +64,56 @@ class ElectronicMailTestCase(ModuleTestCase):
     """
     module = 'electronic_mail'
 
-    def setUp(self):
-        super(ElectronicMailTestCase, self).setUp()
+    # def setUp(self):
+    #     self.Mailbox = POOL.get('electronic.mail.mailbox')
+    #     self.Mail = POOL.get('electronic.mail')
+    #     self.ModelData = POOL.get('ir.model.data')
+    #     self.User = POOL.get('res.user')
 
-        self.Mailbox = POOL.get('electronic.mail.mailbox')
-        self.Mail = POOL.get('electronic.mail')
-        self.ModelData = POOL.get('ir.model.data')
-        self.User = POOL.get('res.user')
-
-    def create_user(self, name):
-        """
-        Creates a new user and returns the ID
-        """
-        group_email_admin_id = self.ModelData.get_id(
-            'electronic_mail', 'group_email_admin')
-        group_email_user_id = self.ModelData.get_id(
-            'electronic_mail', 'group_email_user')
-
-        return self.User.create([
-            {
-            'login': name,
-            'name': name,
-            'password': name,
-            'groups': [('add', [group_email_admin_id, group_email_user_id])]
-            }])[0]
-
-    def create_users(self, no_of_sets=1):
-        """
-        A set of user is:
-            1 Owner
-            1 Read user
-            1 Write User
-        :return: List of tuple of the ID of three users
-        """
-        created_users = []
-        for iteration in xrange(1, no_of_sets + 1):
-            created_users.append(
-                tuple([
-                    self.create_user(user_type % iteration)
-                        for user_type in USER_TYPES
-                ])
-            )
-        return created_users
-
-    def test0010mailbox_read_rights(self):
+    @with_transaction()
+    def test_mailbox_read_rights(self):
         '''
         Read access rights Test
         '''
-        with Transaction().start(DB_NAME, USER, CONTEXT) as transaction:
-            # Create Users for testing access
-            user_set_1, user_set_2 = self.create_users(no_of_sets=2)
-            # Create a mailbox with a user set
-            self.Mailbox.create([
-                {
+        pool = Pool()
+        Mailbox = pool.get('electronic.mail.mailbox')
+
+        # Create Users for testing access
+        user_set_1, user_set_2 = create_users(no_of_sets=2)
+
+        # Create a mailbox with a user set
+        Mailbox.create([{
                     'name': 'Parent Mailbox',
                     'user': user_set_1[0],
                     'read_users': [('add', [user_set_1[1]])],
                     'write_users': [('add', [user_set_1[2]])],
                     }])
 
-            # Create a mailbox 2 with RW users of set 1 + set 2
-            self.Mailbox.create([
-                {
+        # Create a mailbox 2 with RW users of set 1 + set 2
+        Mailbox.create([{
                     'name': 'Child Mailbox',
                     'user': user_set_2[0],
                     'read_users': [('add', [user_set_1[1], user_set_2[1]])],
                     'write_users': [('add', [user_set_1[2], user_set_2[2]])],
                     }])
 
-            # Directly test the mailboxes each user has access to
-            expected_results = {
-                USER: 2,
-                user_set_1[0].id: 2, #1,
-                user_set_2[0].id: 2, #1,
-                user_set_1[1].id: 2,
-                user_set_2[1].id: 2, #1,
-                user_set_1[2].id: 2,
-                user_set_2[2].id: 2, #1,
-                }
-            for user_id, mailbox_count in expected_results.items():
-                with Transaction().set_user(user_id):
-                    mailboxes = self.Mailbox.search([], count=True)
-                    self.assertEqual(mailboxes, mailbox_count)
+        # Directly test the mailboxes each user has access to
+        expected_results = {
+            Transaction().user: 2,
+            user_set_1[0].id: 2,  # 1,
+            user_set_2[0].id: 2,  # 1,
+            user_set_1[1].id: 2,
+            user_set_2[1].id: 2,  # 1,
+            user_set_1[2].id: 2,
+            user_set_2[2].id: 2,  # 1,
+            }
+        for user_id, mailbox_count in expected_results.items():
+            with Transaction().set_user(user_id):
+                mailboxes = Mailbox.search([], count=True)
+                self.assertEqual(mailboxes, mailbox_count)
 
-            transaction.rollback()
-
-    def test0020mail_create_access_rights(self):
+    @with_transaction()
+    def test_mail_create_access_rights(self):
         """
         Create access rights Test
 
@@ -125,49 +125,49 @@ class ElectronicMailTestCase(ModuleTestCase):
 
         Try various security combinations for create
         """
-        with Transaction().start(DB_NAME, USER, CONTEXT) as transaction:
-            user_o, user_r, user_w = self.create_users(no_of_sets=1)[0]
-            mailbox = self.Mailbox.create([
-                {
+        pool = Pool()
+        Mail = pool.get('electronic.mail')
+        Mailbox = pool.get('electronic.mail.mailbox')
+
+        user_o, user_r, user_w = create_users(no_of_sets=1)[0]
+        mailbox = Mailbox.create([{
                     'name': 'Mailbox',
                     'user': user_o,
                     'read_users': [('add', [user_r])],
                     'write_users': [('add', [user_w])],
                     }])[0]
 
-            # Raise exception when writing a mail with the read user
-            with Transaction().set_user(user_r):
-                self.assertRaises(
-                    Exception, self.Mail.create,
-                    ([{
+        # Raise exception when writing a mail with the read user
+        with Transaction().set_user(user_r):
+            self.assertRaises(Exception, Mail.create, [{
                         'from_': 'Test',
                         'mailbox': mailbox,
-                        }]))
+                        }])
 
-            # Creating mail with the write user
-            with Transaction().set_user(user_w):
-                self.assert_(self.Mail.create, ([{
+        # Creating mail with the write user
+        with Transaction().set_user(user_w):
+            self.assert_(Mail.create, [{
                         'from_': 'Test',
                         'mailbox': mailbox.id,
-                    }])
-                )
-
-            # Create an email as mailbox owner
-            with Transaction().set_user(user_o):
-                self.assert_(
-                    self.Mail.create, ([{
-                            'from_': 'Test',
-                            'mailbox': mailbox.id,
                         }])
-                )
 
-            transaction.rollback()
+        # Create an email as mailbox owner
+        with Transaction().set_user(user_o):
+            self.assert_(Mail.create, [{
+                        'from_': 'Test',
+                        'mailbox': mailbox.id,
+                        }])
 
-    def test0030_email_message_extraction(self):
+    @with_transaction()
+    def test_email_message_extraction(self):
         """
         Create from email functionality extracts info from
         an email.message. Test the extraction for multiple types
         """
+        pool = Pool()
+        Mail = pool.get('electronic.mail')
+        Mailbox = pool.get('electronic.mail.mailbox')
+
         # 1: multipart/alternative
         message = MIMEMultipart('alternative')
 
@@ -195,26 +195,23 @@ class ElectronicMailTestCase(ModuleTestCase):
         message.attach(part1)
         message.attach(part2)
 
-        with Transaction().start(DB_NAME, USER, CONTEXT):
-            mailbox = self.Mailbox.create([
-                {
+        current_user = Transaction().user
+        mailbox = Mailbox.create([{
                     'name': 'Mailbox',
-                    'user': USER,
-                    'read_users': [('add', [USER])],
-                    'write_users': [('add', [USER])],
+                    'user': current_user,
+                    'read_users': [('add', [current_user])],
+                    'write_users': [('add', [current_user])],
                     }])[0]
-            mail = self.Mail.create_from_mail(message, mailbox)
+        mail = Mail.create_from_mail(message, mailbox)
 
-            self.assertEqual(mail.subject, message['Subject'])
-            self.assertEqual(mail.from_, message['From'])
-            self.assertEqual(mail.to, message['To'])
+        self.assertEqual(mail.subject, message['Subject'])
+        self.assertEqual(mail.from_, message['From'])
+        self.assertEqual(mail.to, message['To'])
 
 
 def suite():
     "Electronic mail test suite"
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        ElectronicMailTestCase
-        )
-    )
+            ElectronicMailTestCase))
     return suite
